421ab7298283df912b470a834e3c3a14a3482834
PJaeHyeon <qkrwogus423@naver.com>
2021-03-09 15:20:42 +0900

    Upload Menu

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Player");
+		System.out.println("2. Delete Player");
+		System.out.println("3. Edit Player");
+		System.out.println("4. View Player");
+		System.out.println("5. Exit");
+		
+	}
+
+}
+
