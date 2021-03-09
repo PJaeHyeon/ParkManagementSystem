4d74345bcb85cb4e6d570a02c7f9b64d3086a642
PJaeHyeon <qkrwogus423@naver.com>
2021-03-09 15:18:02 +0900

    upload test

--- src/test.java
+++ src/test.java
@@ -2,10 +2,12 @@
 public class test {
 
 	public static void main(String[] args) {
-
-
-		System.out.println("Test!");
-
+		System.out.println("1. Add Player");
+		System.out.println("2. Delete Player");
+		System.out.println("3. Edit Player");
+		System.out.println("4. View Player");
+		System.out.println("5. Exit");
+		
 	}
 
 }
