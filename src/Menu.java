ad6caa18af9cdd3e1657eda8476fd04141e709a1
PJaeHyeon <80306229+PJaeHyeon@users.noreply.github.com>
2021-03-20 14:33:54 +0900

    Update Menu.java
    try

--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,13 @@
+
 import java.util.Scanner;
 
-public class MenuManager {
+
+public class ManageMent {
 
 	public static void main(String[] args) {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
+		
 		
 		while(num != 6){
 			System.out.println("================================"); //Input Layout
@@ -14,35 +17,49 @@ public class MenuManager {
 			System.out.println("4. View Player                 *"); //View Player 출력
 			System.out.println("5. Show a Menu                 *"); //Show a Menu 출력
 			System.out.println("6. Exit                        *"); //Exit 출력
-			System.out.println("Select one number between 1-6: *"); //메뉴 넘버 유도 문장 출력
 			System.out.println("================================"); //Input Layout
+			
+			System.out.print("Select one number between 1-6: "); //메뉴 넘버 유도 문장 출력
 			num = input.nextInt(); //num 입력 int 변수 선언
-			switch(num) {        //num입력에 따른 스위치문
+			
+			int playernum , playersal;	
+			String playername,playerteam;
+			
+			switch(num){						//num입력에 따른 스위치문
+				
 			case 1:
 				System.out.print("Player Number: "); 
-				int playernum = input.nextInt(); //선수 번호 입력
+				playernum = input.nextInt(); //선수 번호 입력
 				System.out.print("Player Name: ");
-				String playername = input.next(); //선수 이름 입력
+				playername = input.next(); //선수 이름 입력
 				System.out.print("Player's Team: ");
-				String playerteam = input.next(); //선수 소속팀 입력
-				System.out.print("Player's Salary");
-				int playersal = input.nextInt(); //선수 연봉 입력
+				playerteam = input.next(); //선수 소속팀 입력
+				System.out.print("Player's Salary: ");
+				playersal = input.nextInt(); //선수 연봉 입력
 				
-				System.out.println(playernum);
-				System.out.println(playername);
-				System.out.println(playerteam);
-				System.out.println(playersal);
-			
+				System.out.printf("Playernum is : %d \n",playernum);
+				System.out.printf("Playername is : %s \n",playername);
+				System.out.printf("Playerteam is : %s \n",playerteam);
+				System.out.printf("PlayerSalary is : %d \n",playersal);
+				
 				break;
+			
 			case 2:
+				
 			case 3:
+				
 			case 4:
+				
 			case 5:
 				
 			}
-			
-			
-		}
+				
+				
+			}
+				
+					
+		
 	}
 
 }
+
