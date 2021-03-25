37107adfe28e99e9328777089d32a1a80479c120
PJaeHyeon <qkrwogus423@naver.com>
2021-03-25 18:57:31 +0900

    test

--- src/test.java
+++ src/test.java
@@ -1,15 +1,45 @@
 
-public class test {
+import java.util.*; 
+public class test { 
+	public static void main(String args[]) { 
+		String poem = "Hold fast to dreams"
+				+ " For if dreams die"
+				+ " Life is a broken-winged bird"
+				+ " That cannot fly"
+				+ " Hold fast to dreams"
+				+ " For when dreams go"
+				+ " Life is a barren field"
+				+ " Frozen with snow";
+		String[] words = poem.split(" ");
+		int wordscount = 0;
+		int setcount = 0;
+		int setcount2 = 0;
 
-	public static void main(String[] args) {
-		System.out.println("1. Add Player");
-		System.out.println("2. Delete Player");
-		System.out.println("3. Edit Player");
-		System.out.println("4. View Player");
-		System.out.println("5. Exit");
 		
-		System.out.println("test 2");
+		HashSet<String> set = new HashSet<String>();
+		for(String a : words) {
+			set.add(a);
+			wordscount++;
+		}
+		for(String s : set) {
+			setcount++;
+		}
+		System.out.println(set);
+		System.out.println("the number of words : " + wordscount);
+		System.out.println("the number of words added to the set is :" +setcount);
 		
-	}
-
+		ArrayList<String> list= new ArrayList<String>();
+		
+		for(String a : words) {
+		list.add(a.toLowerCase());
+		}
+		HashSet<String> set2 = new HashSet<String>();
+			set2.addAll(list);
+		
+		System.out.println(set2);
+		for(String s2 : set2) {
+			setcount2++;
+		}
+		System.out.println("the number of lower case words added to the set2 is :" +setcount2);
+	} 
 }
