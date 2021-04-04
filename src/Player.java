a5af1bece3892ed41d0923c30ea0e1200135cc6d
PJaeHyeon <80306229+PJaeHyeon@users.noreply.github.com>
2021-04-04 13:50:17 +0900

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,29 @@
 
 public class Player {
+	
+	String name;
+	int number;
+	String team;
+	String sal;
+	
+	public Player() {
+	}
+	
+	public Player(String name,int number) 
+	{
+		this.name = name;
+		this.number= number;
+	}
+	
+	public Player(String name, int number, String team, String sal) {
+		this.name = name;
+		this.number = number;
+		this.team=team;
+		this.sal=sal;
+	}
+	
+	public void printInfo() {
+		System.out.println("Name :" + name + ", Number:"+number+", Team:"+team+", Salary:"+sal+"억원");
+	}
 
 }
